0b263cd746e59a2f30672f530c25fa220648d12d
lsaudon <lsaudon@protonmail.com>
2020-03-20 15:41:01 +0100

    remove unused code

--- TennisKata/TennisGame2.cs
+++ TennisKata/TennisGame2.cs
@@ -109,22 +109,6 @@
             return score;
         }
 
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
         private void P1Score()
         {
             _p1Point++;
